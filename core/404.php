1dbf57f342d23987d1ef0b34359ae39eb00784c9
dg239 <mail@thijstervelde.com>
2013-10-26 20:00:40 -0700

    This file is version number 1

---
+++ core/404.php
@@ -0,0 +1,13 @@
+<?php 
+include 'core/init.php';
+include 'includes/overall/header.php'; 
+?>
+<center>
+<h1> Something went wrong and the page could not be showed </h1>
+<h3> Have a raving dino instead</h3>
+<img src="http://25.media.tumblr.com/tumblr_m5s7nbovo41rp0jwzo1_500.gif">
+</center>
+
+
+<?php
+include 'includes/overall/footer.php'; ?> 
