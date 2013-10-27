fa4460df49cf65bff8ca98e24f1e1ebc76fb3e28
dg239 <mail@thijstervelde.com>
2013-10-26 20:00:49 -0700

    This file is version number 1

---
+++ includes/head.php
@@ -0,0 +1,7 @@
+<head>
+   <title>DG239 Assignment Enviroment</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/screen.css">
+	<script type="text/javascript" src="core/js/jquery-2.0.3.min.js"></script>
+//as
+</head>
