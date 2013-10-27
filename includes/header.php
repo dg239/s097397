b330c9ed2cb5ba2db0892fd1d900e1b7c40cce04
dg239 <mail@thijstervelde.com>
2013-10-26 20:00:49 -0700

    This file is version number 1

---
+++ includes/header.php
@@ -0,0 +1,7 @@
+ <header>
+ <embed src="includes/images/logo.svg" type="image/svg+xml" align="right" />
+    <h1 class="logo">Assignment Enviroment</h1>   
+    <?php include 'includes/menu.php'; ?> 
+    <div class="clear"></div>
+//as
+</header>
