230cb1bc69cc25b24ec87fd8e374f43dd0196b76
dg239 <mail@thijstervelde.com>
2013-10-26 20:00:39 -0700

    This file is version number 1

---
+++ tutorial.php
@@ -0,0 +1,9 @@
+<?php 
+include 'core/init.php';
+protect_page();
+include 'includes/overall/header.php'; 
+?> 
+      <h1>Revisions</h1>
+      <p>Just some revisions, you know, chilling</p>
+
+<?php include 'includes/overall/footer.php'; ?> 
