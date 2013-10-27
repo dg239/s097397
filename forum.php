52812dcff5e1a1677d4d1d6d33ccf6ab8af7aa6e
dg239 <mail@thijstervelde.com>
2013-10-26 20:00:34 -0700

    This file is version number 1

---
+++ forum.php
@@ -0,0 +1,9 @@
+<?php 
+include 'core/init.php';
+protect_page();
+include 'includes/overall/header.php'; 
+?> 
+      <h1>forum</h1>
+      <p>General area for the forum.</p>
+
+<?php include 'includes/overall/footer.php'; ?> 
