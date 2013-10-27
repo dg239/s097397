8ca5c5ce091cf5b9ce8c48273cf3bea79d9e0ee7
dg239 <mail@thijstervelde.com>
2013-10-26 20:00:30 -0700

    This file is version number 1

---
+++ _test6.php
@@ -0,0 +1,11 @@
+<?php 
+include 'core/init.php';
+include 'includes/overall/header.php'; 
+$snumber= 's0983456';
+echo $userID 	= userID_from_data($snumber,'snumber');
+?> 
+
+
+<?php include 'includes/overall/footer.php'; ?> 
+
+asdasdasdasdasdasdas
