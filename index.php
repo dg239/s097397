c60e25390165c6a82902505b38050a34839ef0bb
dg239 <mail@thijstervelde.com>
2013-10-26 20:00:35 -0700

    This file is version number 1

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+include 'core/init.php';
+include 'includes/overall/header.php'; 
+?> 
+      <h1>Featured homepage</h1>
+      <p>Currently worked on projects</p>
+      <h1>Previous lessons</h1>
+      <p>Files and links</p>
+<?php include 'includes/overall/footer.php'; ?> 
