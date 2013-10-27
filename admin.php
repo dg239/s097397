33d7b620c1c05736c6f47024629dadf57ac04d4e
dg239 <mail@thijstervelde.com>
2013-10-26 20:00:31 -0700

    This file is version number 1

---
+++ admin.php
@@ -0,0 +1,10 @@
+<?php 
+include 'core/init.php';
+include 'includes/overall/header.php'; 
+protect_page();
+admin_protect();
+?> 
+      <h1>Admin page</h1>
+      <p>Admin page</p>
+
+<?php include 'includes/overall/footer.php'; ?> 
